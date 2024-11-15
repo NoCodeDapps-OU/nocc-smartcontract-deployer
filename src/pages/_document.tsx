d126db8022381fc9cba1ada474f75b3d153e21d2
alirazacodes <aliraza.ali954@gmail.com>
2024-11-15 11:19:10 -0800

    fixed/ font preload console error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
         />
         <link rel="icon" href="/nocc-logo.png" type="image/png" />
       </Head>
